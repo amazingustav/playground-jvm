05be98736f7c2cd769d230cb6cc97ff91b4c4fae
Gustavo Amorim <gustavoamorim02@gmail.com>
2023-03-10 23:32:32 -0300

    Create balance brackets function and test

---
+++ src/main/kotlin/br/com/amazingapps/playground/BalanceBrackets.kt
@@ -0,0 +1,67 @@
+package br.com.amazingapps.playground
+
+/**
+ * Given a string with brackets -> (, ), [, ], { and } you must return `true` if it is a valid string or `false` if it isn't.
+ * A valid String in that structure means that ALL BRACKETS can be closed correctly.
+ *
+ * E.g.
+ * (([])) -> valid
+ * ((][)) -> invalid
+ * ({[]}) -> valid
+ * {{{[[]](()[]}}} -> invalid
+ * f(e(0)) -> valid
+ * */
+class BalanceBrackets {
+
+    fun isBalanced(brackets: String): Boolean {
+        var s = brackets
+
+        while (s.isNotEmpty()) {
+            val temp = s
+            s = s.replace(Regex("[^()\\[\\]{}]"), "")
+                .replace("()", "")
+                .replace("[]", "")
+                .replace("{}", "");
+
+            if (s == temp) break
+        }
+
+        return s.isEmpty()
+    }
+
+    fun isBalancedAlternative(s: String): Boolean {
+        if (s.first().isCloseBracket() || s.last().isOpenBracket()) return false
+
+        val openBrackets = mutableListOf<Char>()
+
+        s.toCharArray().forEach {
+            if (it.isCloseBracket()) {
+                val lastOpenBracket = if (openBrackets.isEmpty()) null else openBrackets[openBrackets.size - 1]
+
+                if (lastOpenBracket?.reverse() != it) {
+                    return false
+                } else openBrackets.removeLast()
+            } else {
+                openBrackets.add(it)
+            }
+        }
+
+        return true
+    }
+
+    private fun Char.reverse() = when {
+        this == '(' -> ')'
+        this == '{' -> '}'
+        this == '[' -> ']'
+        this == ')' -> '('
+        this == '}' -> '{'
+        this == ']' -> '['
+        else -> throw IllegalArgumentException("Character must be a bracket!")
+    }
+
+    private fun MutableList<Char>.removeLast() = this.removeAt(this.size - 1)
+
+    private fun Char.isCloseBracket() = this == '}' || this == ']' || this == ')'
+
+    private fun Char.isOpenBracket() = this == '{' || this == '[' || this == '('
+}
---
+++ src/test/kotlin/br/com/amazingapps/playground/BalanceBracketsTest.kt
@@ -0,0 +1,27 @@
+package br.com.amazingapps.playground
+
+import br.com.amazingapps.playground.BalanceBrackets
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import java.lang.IllegalArgumentException
+
+class BalanceBracketsTest {
+
+    private val balanceBrackets: BalanceBrackets = BalanceBrackets()
+    @Test
+    fun balancedBracketsShouldReturnTrue() {
+        val data = "f(e(d))"
+
+        assert(balanceBrackets.isBalanced(data))
+    }
+
+    @Test
+    fun bracketsWithLettersShouldThrowAnError() {
+        val data = "f(e(d))"
+
+        assertThrows(IllegalArgumentException::class.java) {
+            balanceBrackets.isBalancedAlternative(data)
+        }
+    }
+}
