1f056611d99a467537e93d798cb3c911ee2be93e
Gustavo Amorim <gustavoamorim02@gmail.com>
2023-09-18 14:50:23 -0300

    Add tests for SmallestPositiveInt

--- src/test/kotlin/br/com/amazingapps/playground/math/SmallestPositiveIntTest.kt
+++ src/test/kotlin/br/com/amazingapps/playground/math/SmallestPositiveIntTest.kt
@@ -1,5 +1,6 @@
-package br.com.amazingapps.playground
+package br.com.amazingapps.playground.math
 
+import br.com.amazingapps.playground.math.SmallestPositiveInt
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
